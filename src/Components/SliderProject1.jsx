85a242b0c88ec045151b56a6b949d44c0f457d77
Aude Sedillo <aude.sedillo@gmail.com>
2024-05-12 23:01:57 +0200

    pro-project pages done + add details responsive and fixed some

---
+++ src/Components/SliderProject1.jsx
@@ -0,0 +1,73 @@
+import lrw_home from "../assets/image/picture-lrw/LRW-Home.png";
+import lrw_sidebar from "../assets/image/picture-lrw/LRW-SideBar.png";
+import lrw_caroussel from "../assets/image/picture-lrw/LRW-Caroussel.png";
+import lrw_semainierheader from "../assets/image/picture-lrw/LRW-Semainier-Header.png";
+import lrw_semainierbody from "../assets/image/picture-lrw/LRW-Semainier-body.png";
+import lrw_recipeheader from "../assets/image/picture-lrw/LRW-Recipe-Header.png";
+import lrw_recipebody from "../assets/image/picture-lrw/LRW-recipe-body.png";
+import lrw_recipecomments from "../assets/image/picture-lrw/LRW-recipe-comments.png";
+
+import "./SliderProject.css";
+import { Link } from "react-router-dom";
+
+function SliderProject1() {
+  return (
+    <section className="display-slider">
+      <h1 className="title-section-website">THE WEBSITE</h1>
+      <section className="section-slider">
+        <img
+          src={lrw_home}
+          alt="picture-les-recettes-wild"
+          className="picture-slider"
+        />
+        <img
+          src={lrw_sidebar}
+          alt="picture-les-recettes-wild"
+          className="picture-slider"
+        />
+        <img
+          src={lrw_caroussel}
+          alt="picture-les-recettes-wild"
+          className="picture-slider"
+        />
+        <img
+          src={lrw_semainierheader}
+          alt="picture-les-recettes-wild"
+          className="picture-slider"
+        />
+        <img
+          src={lrw_semainierbody}
+          alt="picture-les-recettes-wild"
+          className="picture-slider"
+        />
+        <img
+          src={lrw_recipeheader}
+          alt="picture-les-recettes-wild"
+          className="picture-slider"
+        />
+        <img
+          src={lrw_recipebody}
+          alt="picture-les-recettes-wild"
+          className="picture-slider"
+        />
+        <img
+          src={lrw_recipecomments}
+          alt="picture-les-recettes-wild"
+          className="picture-slider"
+        />
+      </section>
+      <section className="section-website-link">
+        <p className="p-website-link">COME HAVE A LOOK ON THE WEBSITE :</p>
+        <Link
+          className="link-website"
+          to="https://maximmft.github.io/Les-recettes-Wild/index.html"
+          target="_blank"
+        >
+          CLICK HERE !
+        </Link>
+      </section>
+    </section>
+  );
+}
+
+export default SliderProject1;
--- src/Pages/ProProjectHome.jsx
+++ src/Pages/ProProjectHome.jsx
@@ -1,22 +1,45 @@
-// import ProProject1 from "../Components/ProProject1";
 import { useNavigate } from "react-router-dom";
 import Footer from "../Components/Footer";
+import "./ProProjectHome.css";
 
 function ProProjectHome() {
-const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    return (
-        <div>
-            <section>
-            <h1>WELCOME ON MY PROJECT, LET'S HAVE A LOOK</h1>
-            </section>
-            <div>
-                <button onClick={() => navigate("/pro-project-1")}>Project 1</button>
-                <button onClick={() => navigate("/pro-project-1")}>Project 2</button>
-            </div>
-            <Footer />
-        </div>
-    );
+  return (
+    <div className="display-pro-home">
+      <section className="section-title-pro-home">
+        <h1 className="title-pro-home">
+          WELCOME ON MY PROJECTS, COME HAVE A LOOK
+        </h1>
+      </section>
+      <section className="section-pro-project-btns">
+        <section
+          onClick={() => navigate("/pro-project-1")}
+          className="section-btn-pro"
+        >
+            <h2 className="title-project-pro-home">PROJECT 1</h2>
+            <p className="title-site-pro">
+              LES
+              <br /> RECETTES
+              <br /> WILD
+            </p>
+        </section>
+        <section
+          onClick={() => navigate("/pro-project-2")}
+          className="section-btn-pro"
+        >
+          <div>
+            <h2 className="title-project-pro-home">PROJECT 2</h2>
+            <p className="title-site-pro">
+              FILM
+              <br /> FUSION
+            </p>
+          </div>
+        </section>
+      </section>
+      <Footer />
+    </div>
+  );
 }
 
 export default ProProjectHome;
