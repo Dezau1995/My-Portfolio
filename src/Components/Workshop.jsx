3272b9f9aff290622960c21bbd28aab09e84d2f1
Aude Sedillo <aude.sedillo@gmail.com>
2024-06-09 13:45:59 +0200

    finish all details + all responsive mobile/tablet

--- src/Components/Workshop.jsx
+++ src/Components/Workshop.jsx
@@ -1,15 +1,9 @@
-import dataHackathon from "../data/hackathon.js";
-import whireframe_P1 from "../assets/image/picture-lrw/whireframe-lesrecetteswild.png";
-import maquette_P1 from "../assets/image/picture-lrw/maquette-lesrecetteswild.png";
 import Footer from "./Footer";
-import SliderProject1 from "./SliderProject1";
 import { motion, useScroll, useSpring } from "framer-motion";
 import "./ProProject.css";
+import SliderWorkshop from "./SliderWorkshop.jsx";
 
 function Workshop() {
-  // console.log(data)
-  const dataPro = dataHackathon;
-
   const { scrollYProgress } = useScroll();
   const scaleX = useSpring(scrollYProgress, {
     stiffness: 100,
@@ -28,25 +22,23 @@ function Workshop() {
         </h1>
       </section>
       <section className="section-presentation">
-        <section className="section-introduction">
+        <section className="section-introduction-description">
           <h2 className="title-introduction">INTRODUCTION</h2>
-          <p className="paragraph-project">{dataPro[0].introduction1}</p>
-          <p className="paragraph-project">{dataPro[0].introduction2}</p>
-          <p className="paragraph-project">{dataPro[0].introduction3}</p>
+          <p className="paragraph-project">
+            I WANTED TO HIGHLIGHT THIS WORKSHOP CAUSE IT WAS USEFUL TO PRATICE
+            SKILLS THAT I DIDN'T USED ON MY PROJECTS, LIKE CREATE A CONTEXT.
+          </p>
+          <p className="paragraph-project">
+            I WORKED WITH MAP AND FILTER METHODS, FETCHING API AND USECONTEXT.
+            CONTEXT HERE IS USEFUL TO CHANGE THEMES (DARK OR LIGHT).
+          </p>
+          <p className="paragraph-project">
+            I WANTED TO HAVE FUN ON RECREATE ALL THE DESIGN OF THIS PROJECT WITH
+            CSS.
+          </p>
         </section>
       </section>
-      <section className="section-whireframe-models">
-        <h3 className="title-section-whireframe">WHIREFRAME & MODELS</h3>
-        <div className="display-image">
-          <img
-            src={dataPro[0].pictureWhireframe}
-            alt="whireframe"
-            className="picture-whireframe"
-          />
-          <img src={maquette_P1} alt="models" className="picture-models" />
-        </div>
-      </section>
-      <SliderProject1 />
+      <SliderWorkshop />
       <Footer />
     </section>
   );
